d1f66db54235b5ba95f3b9b3cb033ab0470e227c
Pedro Henrique Avila <wanessakcmelo@gmail.com>
2023-10-04 12:46:42 -0300

    React-rout-dom

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import './Home.css';
+import { useNavigate, Link } from 'react-router-dom'
+import React from 'react'
 
-function Home(){
+
+
+const Home = () => {
+    let navigate = useNavigate()
     return (
-        <>
-            <h1 >Home</h1>
-           
-        </>
-    );
+
+        <div>
+            <h2 className="text-slate-900 text-5xl  m-4">Home</h2>
+            <div>
+                <button type='submit'
+                    className='hover:underline mx-4'
+                    onClick={() => { navigate('/login') }}>
+                    Login useNavigate
+                </button>
+                <Link to='/login' className='hover:underline mx-4'>Login por Link</Link>
+            </div>
+
+        </div>
+
+    )
 }
 
-export default Home;
+export default Home
