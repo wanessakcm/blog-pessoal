277ae88a41a0a9f592fb0a33aeb292d17a145c34
Pedro Henrique Avila <wanessakcmelo@gmail.com>
2023-10-16 14:17:01 -0300

    React- implement Post, Put e Delete - Tema e Postagem

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import homeLogo from '../../assets/1696437213740.png'
+import homeLogo from '../../assets/home.png'
 import './Home.css';
+import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens';
+import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
+
 
 
 function Home() {
@@ -11,20 +14,20 @@ function Home() {
             <div className="flex flex-col gap-4 items-center justify-center py-4">
               <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
               <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
-
+  
               <div className="flex justify-around gap-4">
-
-                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
-              </div>
+              <ModalPostagem />
+              <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
             </div>
-
+            </div>
+  
             <div className="flex justify-center ">
               <img src={homeLogo} alt="" className='w-2/3' />
-
+      
             </div>
           </div>
         </div>
-
+        <ListaPostagens />
       </>
     );
 }
