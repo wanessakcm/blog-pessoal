90f069ed80ee39b03d37e1afa55e04ba0edd6640
Pedro Henrique Avila <wanessakcmelo@gmail.com>
2023-10-23 22:39:59 -0300

    versao final

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+
+
 import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
 import Login from './paginas/login/Login';
